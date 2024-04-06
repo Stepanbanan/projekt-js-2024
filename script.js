220a1eca4796baf4d9e54f1456f3efcc60c3406d
Stepanbanan <131649103+Stepanbanan@users.noreply.github.com>
2024-04-06 14:23:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ let gameStarted = false;
 let player1Choice = null;
 let player2Choice = null;
 
+
 function startGame() {
     const player1Input = document.getElementById('player1Name').value.trim();
     const player2Input = document.getElementById('player2Name').value.trim();
+
+    document.getElementById('player2').classList.add('hidden');
 
     if (!player1Input || !player2Input) {
         alert("Lūdzu, ievadiet savus vārdus, lai sāktu spēli");
@@ -18,7 +21,7 @@ function startGame() {
     document.getElementById('gameArea').classList.remove('hidden');
     document.getElementById('player1Display').textContent = player1Name;
     document.getElementById('player2Display').textContent = player2Name;   
-    resultDisplay.textContent = `${player1Name}, izvelies!`;
+    document.getElementById('result').textContent = `${player1Name}, izvelies!`;
 }
 
 function makeChoice(player, choice) {
@@ -28,33 +31,40 @@ function makeChoice(player, choice) {
 
     if (player === 'player1') {
         player1Choice = choice;
+        document.getElementById('player2').classList.remove('hidden');
     } else {
         player2Choice = choice;
     }
 
+    document.getElementById('player1').classList.add('hidden');
     resultDisplay.textContent = `${Player2Name}, izvēlies!`;
 
     if (player1Choice !== null && player2Choice !== null) {
         determineWinner();
+        document.getElementById('player1').classList.add('hidden');
+        document.getElementById('player2').classList.add('hidden');
     }
+
 }
 
 function determineWinner() {
     const resultDisplay = document.getElementById('result');
+    document.getElementById('container4').classList.remove('hidden');
 
     if (player1Choice === player2Choice) {
-        resultDisplay.textContent = "neisčirts";
+        resultDisplay.textContent = "Neisčirts";
     } else if (
         (player1Choice === 'akmens' && player2Choice === 'šķēres') ||
         (player1Choice === 'papirs' && player2Choice === 'akmens') ||
         (player1Choice === 'šķēres' && player2Choice === 'papirs')
     ) {
-        resultDisplay.textContent = `${player1Name} uzvareja!`;
+        resultDisplay.textContent = `${player1Name} Uzvareja!`;
     } else {
-        resultDisplay.textContent = `${player2Name} uzvareja!`;
+        resultDisplay.textContent = `${player2Name} Uzvareja!`;
     }
     player1Choice = null;
     player2Choice = null;
+
 }
 
 var timeDisplay = document.getElementById("time");
